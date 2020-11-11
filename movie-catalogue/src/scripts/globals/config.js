f3ea5a891dcac98e274b895f71bf0a456ea15e66
Muhammad Aziz Al-assad <muhazizal.26@gmail.com>
2020-11-11 08:01:34 +0700

    Add files via upload

---
+++ movie-catalogue/src/scripts/globals/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+	KEY: 'a9739b3415c360505a7d1db19829fe66',
+	BASE_URL: 'https://api.themoviedb.org/3/',
+	BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+	DEFAULT_LANGUAGE: 'en-us',
+	CACHE_NAME: new Date().toISOString(),
+	DATABASE_NAME: 'movie-catalogue-database',
+	DATABASE_VERSION: 1,
+	OBJECT_STORE_NAME: 'movies',
+	WEB_SOCKET_SERVER: 'wss://stormy-badlands-06326.herokuapp.com',
+};
+
+export default CONFIG;
