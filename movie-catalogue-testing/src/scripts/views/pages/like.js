0e7a5a210571509f032120ae56ec0d70e17bd55c
Muhammad Aziz Al-assad <muhazizal.26@gmail.com>
2020-11-11 08:12:55 +0700

    Add files via upload

---
+++ movie-catalogue-testing/src/scripts/views/pages/like.js
@@ -0,0 +1,21 @@
+import FavoriteMovieIdb from '../../data/favorite-movie-idb';
+import FavoriteMovieSearchView from './liked-movies/favorite-movie-search-view';
+import FavoriteMovieSearchPresenter from './liked-movies/favorite-movie-search-presenter';
+import FavoriteMovieShowPresenter from './liked-movies/favorite-movie-show-presenter';
+
+const view = new FavoriteMovieSearchView();
+
+const Like = {
+	async render() {
+		return view.getTemplate();
+	},
+
+	async afterRender() {
+		// eslint-disable-next-line no-new
+		new FavoriteMovieShowPresenter({ view, favoriteMovies: FavoriteMovieIdb });
+		// eslint-disable-next-line no-new
+		new FavoriteMovieSearchPresenter({ view, favoriteMovies: FavoriteMovieIdb });
+	},
+};
+
+export default Like;
